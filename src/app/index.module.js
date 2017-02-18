0bd7f913e91b6d34a5a382f57aa9f3c426ffeccd
VadossM <vadossm82@gmail.com>
2017-02-18 22:30:40 +0200

    Add side bar

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,6 +2,6 @@
   'use strict';
 
   angular
-    .module('angularProject', ['ngAnimate', 'ngCookies', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ngMaterial', 'toastr']);
+    .module('angularProject', ['ngAnimate', 'ngCookies', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ui.router', 'ngMaterial', 'toastr', 'ui.tree']);
 
 })();
--- src/app/index.route.js
+++ src/app/index.route.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
   'use strict';
 
   angular
@@ -6,60 +6,24 @@
     .config(routerConfig);
 
   /** @ngInject */
-  function routerConfig($stateProvider/*, $urlRouterProvider*/) {
+  function routerConfig($stateProvider, $urlRouterProvider) {
     $stateProvider
-      .state('login', {
-        url: '/login',
-        templateUrl: 'app/components/login/login.html',
-        controller: 'LoginController',
-        controllerAs: 'user'
-      })
-      .state('registrate', {
-        url: '/registrate',
-        templateUrl: 'app/components/registrate/registrate.html',
-        controller: 'RegistrateController',
-        controllerAs: 'reg'
-      })
-      .state('old', {
-        url: '/old',
-        templateUrl: 'app/main/main.html'
-      })
-      .state('projects', {
+      .state('home', {
         url: '/',
-        views: { 
-          "": { templateUrl: 'app/components/projects/projects.html' },
-          "projectList@projects": { templateUrl: 'app/components/projects/projectlist.html' },
-          "userData@projects": { templateUrl: 'app/components/projects/userdata.html' }
-        }
-      })
-      .state('projectsItem', {
-        url: "/project/:projectId",
-        templateUrl: 'app/components/projects/project_item.html'
-      })
-      .state('projectsItem.inner', {
-        url: "/tasklist",
-        templateUrl: 'app/components/projects/tasklist.tpl.html',
-        controller: 'ProjectPageInnerController',
-        controllerAs: 'tasklist'
-      })
-      .state('directives', {
-        url: "/dirs",
-        templateUrl: 'app/components/directives/directives.html'
-      })
-      .state('slider', {
-        url: "/slider",
-        templateUrl: 'app/components/slider/slider.html'
-      })
-      .state('films', {
-        url: "/films",
-        templateUrl: 'app/components/filters/films.html'
-      })
-      .state("otherwise", {
+        templateUrl: 'app/components/shop-home/home.html',
+        controller: 'HomeController',
+        controllerAs: 'home'
+        // views: { 
+        //   "": { templateUrl: 'app/components/projects/projects.html' },
+        //   "projectList@projects": { templateUrl: 'app/components/projects/projectlist.html' },
+        //   "userData@projects": { templateUrl: 'app/components/projects/userdata.html' }
+      });
+     /* .state("otherwise", {
         url: "*path",
         templateUrl: "app/main/error.html"
-      });
+      });*/
 
-    //$urlRouterProvider.otherwise('/login');
+    $urlRouterProvider.otherwise('/');
   }
 
 })();
